7c9914ca2a6a78aa54d0833e9f901afede9019b2
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-03-23 10:47:53 +0200

    feature: basic grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from "react";
-
 import "./App.css";
+
+const images: string[] = [
+    "https://source.unsplash.com/300x300/?nature,water",
+    "https://source.unsplash.com/300x300/?thunder",
+    "https://source.unsplash.com/300x300/?game",
+    "https://source.unsplash.com/300x300/?nintendo",
+    "https://source.unsplash.com/300x300/?food",
+    "https://source.unsplash.com/300x300/?computer"
+];
 
 function App() {
     return (
         <div className="App">
-            <div className="gallery">gallery</div>
+            <div className="gallery">
+                {images.map(image => (
+                    <img src={image} alt="gallery-item" className="gallery-card"></img>
+                ))}
+            </div>
         </div>
     );
 }
