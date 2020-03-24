c9e8b3a40726e060c4574a627ffcb3da45c4b49e
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-03-24 17:04:18 +0200

    feat: styling + linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./css/App.css";
 import {Gallery} from "./components/Gallery";
+import {Header} from "./components/Header";
 
 function App() {
     return (
         <div className="App">
+            <Header></Header>
             <Gallery></Gallery>
         </div>
     );
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <button>Upload</button>
+            <p>Firebase Gallery</p>
+            <p>Theme</p>
+        </div>
+    );
+};
