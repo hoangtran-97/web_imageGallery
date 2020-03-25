f60d1954afdc8fb22a93b96c04a5d3047d7345cf
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-03-25 15:26:53 +0200

    feat: loading modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./css/App.css";
 import {Gallery} from "./components/Gallery";
 import {Header} from "./components/Header";
+import {Loading} from "./components/Loading";
 
 function App() {
     return (
         <div className="App">
             <Header></Header>
             <Gallery></Gallery>
+            <Loading></Loading>
         </div>
     );
 }
