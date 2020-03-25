f3db5ccfd1ad4e7c7a0e4105a8f1501e55436720
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-03-25 10:28:22 +0200

    feat: vibrant functioning

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div className="header">
+            <button id="upload">Upload</button>
+            <p>Firebase Gallery</p>
+            <p>Github</p>
+        </div>
+    );
+};
