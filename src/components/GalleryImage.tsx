2985897511825e91bbee777ea75b78840d080444
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-03-25 08:33:26 +0200

    feat: added vibrant

--- src/components/GalleryImage.tsx
+++ src/components/GalleryImage.tsx
@@ -1,12 +1,17 @@
 import React from "react";
+const vibrant = require("node-vibrant");
 interface GalleryImageProps {
     image: {id: number; link: string};
     key: string;
 }
 export const GalleryImage = ({image}: GalleryImageProps) => {
     const {link} = image;
-    return <div style={{backgroundImage: `url(${link})`}} className="gallery-card" onClick={updateColor}></div>;
+    return (
+        <div style={{backgroundImage: `url(${link})`}} className="gallery-card" onClick={() => updateColor(link)}></div>
+    );
 };
-const updateColor = () => {
-    console.log("updated");
+const updateColor = (link: string) => {
+    vibrant.from(link).getPalette((err: any, palette: any): any => {
+        console.log(palette, err);
+    });
 };
