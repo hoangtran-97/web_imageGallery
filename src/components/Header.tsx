bf8178a6c03e444c1a771f78205b4dcef7664027
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-03-25 11:42:35 +0200

    styling: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import React from "react";
 export const Header = () => {
     return (
         <div className="header">
-            <button id="upload">Upload</button>
-            <p>Firebase Gallery</p>
-            <p>Github</p>
+            <button className="header-button">Upload</button>
+            <p id="header-text">Firebase Gallery</p>
+            <button className="header-button">Github</button>
         </div>
     );
 };
