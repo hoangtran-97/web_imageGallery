d07fa96e155bc723e5d9a69a315e1244399d27c3
HoangTran <40290572+hoangtran-97@users.noreply.github.com>
2020-03-27 10:49:30 +0200

    styling: header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,22 @@
 import React from "react";
+import {FaGithub, FaCloudUploadAlt} from "react-icons/fa";
 interface HeaderProps {
     setUploadModalOpen: Function;
 }
 export const Header = ({setUploadModalOpen}: HeaderProps) => {
     return (
         <div className="header">
-            <button className="header-button" onClick={() => setUploadModalOpen(true)}>
+            <button className="header-button hvr-icon-wobble-vertical" onClick={() => setUploadModalOpen(true)}>
+                <FaCloudUploadAlt className="header-icon hvr-icon"></FaCloudUploadAlt>
                 Upload
             </button>
             <p id="header-text">Firebase Gallery</p>
-            <button className="header-button">Github</button>
+            <button className="header-button hvr-icon-wobble-vertical">
+                <a href="https://github.com/hoangtran-97/web_imageGallery" target="_blank" rel="noopener noreferrer">
+                    <FaGithub className="header-icon  hvr-icon"></FaGithub>
+                    GitHub
+                </a>
+            </button>
         </div>
     );
 };
